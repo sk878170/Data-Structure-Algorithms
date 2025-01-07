342ea9b390a659a3bd2429b22f537a0fa277dd02
Shubham Kumar <Sk@gmail.com>
2025-01-07 16:01:12 +0530

    Add user input functionality to removeDuplicates method in Solution.java

--- rem_duplicate/Solution.java
+++ rem_duplicate/Solution.java
@@ -1,19 +1,34 @@
 package rem_duplicate;
 
 public class Solution {
-    public  int removeDuplicates(int[] nums) {
-        int i=0;
-
-        for (final int num:nums){
-            if (i<1||num>nums[i-1]){
-                nums[i++]=num;
-                
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0; // If the array is empty, return 0.
         }
-        
+
+        int i = 0; // Pointer for the position of unique elements.
+
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) { // When a new unique element is found
+                i++; // Move the pointer to the next position
+                nums[i] = nums[j]; // Copy the unique element to the new position
+            }
+        }
+
+        return i + 1; // Return the count of unique elements
     }
-    return i;
-    
-}
-}
 
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1, 1, 2, 2, 3, 3, 4, 5, 5};
 
+        int k = solution.removeDuplicates(nums);
+
+        System.out.println("Number of unique elements: " + k);
+        System.out.print("Modified array with unique elements: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
+    }
+}
--- rem_duplicate/Solution.py
+++ rem_duplicate/Solution.py
@@ -1,20 +1,20 @@
 from typing import List
 
-
 class Solution:
     def remove_duplicates(self, nums: List[int]) -> int:
         if not nums:
-            return 0
-        i = 0
-        for num in nums:
-            if i<1 or num>nums[i-1]:
-                nums[i] = num
+            return 0  # Handle empty list case
+        i = 0  # Pointer for the position of unique elements
+        for j in range(1, len(nums)):
+            if nums[j] != nums[i]:  # If a new unique element is found
                 i += 1
-        return i + 1
+                nums[i] = nums[j]  # Move the unique element to the correct position
+        return i + 1  # Return the count of unique elements
 
 # Test the Solution
 solution = Solution()
-print("Enter numbers sepearted by spaces")
-nums =list(map(int, input().split()))
-result=solution.remove_duplicates(nums)
-print(result)
+print("Enter numbers separated by spaces (sorted array):")
+nums = list(map(int, input().split()))  # Input a sorted list
+result = solution.remove_duplicates(nums)
+print("Number of unique elements:", result)
+print("Modified array with unique elements:", nums[:result])
