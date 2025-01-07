d1d3552766a1f7d8129b8f79d0c7cbe5990b6a48
Shubham Kumar <Sk@gmail.com>
2025-01-07 16:01:12 +0530

    Add user input functionality to removeDuplicates method in Solution.java

--- rem_duplicate/solution.cpp
+++ rem_duplicate/solution.cpp
@@ -3,28 +3,57 @@ The order of elements may be changed. Then return the number of elements in nums
 consider no of elements not equal to val as 'k', to get accepted, do the following
 1. Change the array nums such that k elemenst of nums contain the lements which are not equal to val. 
 2. return k. */
+
+
 #include <iostream>
 #include <vector>
 using namespace std;
 
-class Solution{
-    public:
-    int removeDuplicates(vector<int>& nums){
-        int i=0;
+class Solution {
+public:
+    int removeOccurrences(vector<int>& nums, int val) {
+        int k = 0; // Pointer for valid elements
 
-        for (const int num:nums){
-            if(i<1||num>nums[i-1]){
-                nums[i++]=num;
+        for (int num : nums) {
+            if (num != val) {
+                nums[k++] = num; // Keep elements that are not equal to val
+            }
         }
-        return i;
-
+        return k; // Number of elements not equal to val
     }
-}
 };
 
-int main(){
+int main() {
     Solution solution;
-    vector<int> test = {1,1,2,2,3,3,4,4,5,5};
-    cout<<solution.removeDuplicates(test)<<endl;
+    vector<int> nums;
+    int n, val;
+
+    // Input the size of the array
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    // Input the array elements
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; ++i) {
+        int element;
+        cin >> element;
+        nums.push_back(element);
+    }
+
+    // Input the value to remove
+    cout << "Enter the value to remove: ";
+    cin >> val;
+
+    // Remove occurrences of the value
+    int k = solution.removeOccurrences(nums, val);
+
+    // Output the result
+    cout << "Number of elements after removal: " << k << endl;
+    cout << "Modified array: ";
+    for (int i = 0; i < k; ++i) {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+
     return 0;
 }
